0cd10d6a69fc05022326b759c7afbafef8176d83
mrudani1712 <2420030591@klh.edu.in>
2025-08-02 09:14:16 +0530

    add

---
+++ StringConceptsDemo.java
@@ -0,0 +1,46 @@
+public class StringConceptsDemo {
+    public static void main(String[] args) {
+        // Interning
+        String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = new String("Hello");
+
+        System.out.println("Interning:");
+        System.out.println("str1 == str2: " + (str1 == str2)); 
+        System.out.println("str1 == str3: " + (str1 == str3));
+        System.out.println("str1.equals(str3): " + str1.equals(str3)); 
+        System.out.println();
+
+    
+        String original = "Java";
+        String modified = original.concat(" Programming");
+
+        System.out.println("Immutability:");
+        System.out.println("original: " + original);
+        System.out.println("modified: " + modified);
+        System.out.println();
+
+        // equals vs ==
+        String a = "Test";
+        String b = new String("Test");
+
+        System.out.println("equals vs ==:");
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a.equals(b): " + a.equals(b)); // true
+        System.out.println();
+
+        // Array of strings manipulation
+        String[] fruits = {"apple", "banana", "cherry"};
+        System.out.println("Original array:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        // Modify array
+        fruits[1] = "blueberry";
+        System.out.println("\nModified array:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
